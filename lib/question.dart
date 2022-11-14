b201ce3ad0187bdde9c8ec39a781e3e44c56af44
Motaz Yusuf <50301755+thisiswezza@users.noreply.github.com>
2022-11-14 17:10:05 +0200

    Add files via upload

---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  String question;
+  bool answer;
+
+  Question({String q, bool a}) {
+    question = q;
+    answer = a;
+  }
+}
+
+//Question newQuestion = Question(q: 'Text', a: true);
